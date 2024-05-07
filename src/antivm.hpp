3d868245f34ea83830c8ca83161fe8e014490e4d
chalapathi <chalapathi@codetantra.in>
2024-05-07 16:49:55 +0530

    review changes.

--- src/antivm.hpp
+++ src/antivm.hpp
@@ -23,73 +23,73 @@ namespace DetectVM
     {
         HKEY hKey = 0;
         long res = RegOpenKeyEx(HKEY_LOCAL_MACHINE, TEXT("SOFTWARE\\VMware, Inc.\\VMware Tools"), 0, KEY_QUERY_VALUE, &hKey);
-        RegCloseKey(hKey);
-        if (res == -1)
+        if (res == ERROR_SUCCESS)
         {
+            RegCloseKey(hKey);
             return true;
         }
         else
         {
-            HKEY hKey;
+            HKEY hKey2;
             LONG openResult = RegOpenKeyExA(
                 HKEY_LOCAL_MACHINE,
                 "HARDWARE\\DESCRIPTION\\System\\BIOS",
                 0,
                 KEY_READ,
-                &hKey);
+                &hKey2);
             if (openResult != ERROR_SUCCESS)
             {
                 return false;
             }
 
-
             DWORD dataSize = 0;
             LONG queryResult = RegQueryValueExA(
-                hKey,
+                hKey2,
                 "BIOSVersion",
                 NULL,
                 NULL,
                 NULL,
                 &dataSize);
-            
-            
+
             if (queryResult != ERROR_SUCCESS)
             {
-                RegCloseKey(hKey);
+                RegCloseKey(hKey2);
                 return false;
             }
 
             LPBYTE dataBuffer = new BYTE[dataSize];
 
             LONG getValueResult = RegGetValueA(
-                hKey,
+                hKey2,
                 NULL,
                 "BIOSVersion",
                 RRF_RT_REG_SZ,
                 NULL,
                 dataBuffer,
                 &dataSize);
-
+            
+            RegCloseKey(hKey2);
             if (getValueResult != ERROR_SUCCESS)
             {
                 delete[] dataBuffer;
-                RegCloseKey(hKey);
                 return false;
             }
-            hKey = 0;
-            res = RegOpenKeyEx(HKEY_LOCAL_MACHINE, TEXT("SOFTWARE\\VMware, Inc.\\VMware Drivers"), 0, KEY_QUERY_VALUE, &hKey);
-            RegCloseKey(hKey);
+            hKey2 = 0;
+            long driversOpenResult = RegOpenKeyEx(HKEY_LOCAL_MACHINE, TEXT("SOFTWARE\\VMware, Inc.\\VMware Drivers"), 0, KEY_QUERY_VALUE, &hKey2);
+            if (driversOpenResult == ERROR_SUCCESS)
+            {
+                RegCloseKey(hKey2);
+            }
 
-            if ( res == ERROR_SUCCESS && (strncmp(reinterpret_cast<const char *>(dataBuffer), "VMW", 3) == 0 || strncmp(reinterpret_cast<const char *>(dataBuffer), "VMware", 6) == 0))
-            { 
+            if (res == ERROR_SUCCESS && (strncmp(reinterpret_cast<const char *>(dataBuffer), "VMW", 3) == 0 || strncmp(reinterpret_cast<const char *>(dataBuffer), "VMware", 6) == 0))
+            {
                 return true;
             }
             else
             {
-                return false; 
+                return false;
             }
             delete[] dataBuffer;
-            RegCloseKey(hKey);
         }
         return false;
     }
