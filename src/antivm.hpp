46bde5f58390f716a08a0531eea77461e62fa172
chalapathi <chalapathi@codetantra.in>
2024-05-09 10:21:04 +0530

    cpuuid_check

--- src/antivm.hpp
+++ src/antivm.hpp
@@ -1,10 +1,92 @@
-#pragma once
-
 #include <windows.h>
 #include <tchar.h>
 #include <stdbool.h>
 
 #include "hyperv.hpp"
+#include <cstdint>
+
+bool cpuid_check() {
+    unsigned int eax, ebx, ecx, edx;
+    char hyper_vendor_id[13];
+
+    __asm__ __volatile__("cpuid"
+                         : "=a" (eax), "=b" (ebx), "=c" (ecx), "=d" (edx)
+                         : "a" (0x1));
+
+    if (ecx & (1 << 31)) {
+        __asm__ __volatile__("cpuid"
+                             : "=a" (eax), "=b" (ebx), "=c" (ecx), "=d" (edx)
+                             : "a" (0x40000000));
+
+        memcpy(hyper_vendor_id + 0, &ebx, 4);
+        memcpy(hyper_vendor_id + 4, &ecx, 4);
+        memcpy(hyper_vendor_id + 8, &edx, 4);
+        hyper_vendor_id[12] = '\0';
+
+        if (!strcmp(hyper_vendor_id, "VMwareVMware")) {
+            return true; 
+        }
+    }
+    
+    return false;
+}
+
+bool bios_version_check()
+{
+    HKEY hKey2;
+    LONG openResult = RegOpenKeyExA(
+        HKEY_LOCAL_MACHINE,
+        "HARDWARE\\DESCRIPTION\\System\\BIOS",
+        0,
+        KEY_READ,
+        &hKey2);
+    if (openResult != ERROR_SUCCESS)
+    {
+        return false;
+    }
+
+    DWORD dataSize = 0;
+    LONG queryResult = RegQueryValueExA(
+        hKey2,
+        "BIOSVersion",
+        NULL,
+        NULL,
+        NULL,
+        &dataSize);
+
+    if (queryResult != ERROR_SUCCESS)
+    {
+        RegCloseKey(hKey2);
+        return false;
+    }
+
+    LPBYTE dataBuffer = new BYTE[dataSize];
+
+    LONG getValueResult = RegGetValueA(
+        hKey2,
+        NULL,
+        "BIOSVersion",
+        RRF_RT_REG_SZ,
+        NULL,
+        dataBuffer,
+        &dataSize);
+
+    RegCloseKey(hKey2);
+    if (getValueResult != ERROR_SUCCESS)
+    {
+        delete[] dataBuffer;
+        return false;
+    }
+
+    if (strncmp(reinterpret_cast<const char *>(dataBuffer), "VMW", 3) == 0 || strncmp(reinterpret_cast<const char *>(dataBuffer), "VMware", 6) == 0)
+    {
+
+        delete[] dataBuffer;
+        return true;
+    }
+    delete[] dataBuffer;
+    return false;
+}
 
 namespace DetectVM
 {
@@ -27,71 +109,8 @@ namespace DetectVM
         {
             RegCloseKey(hKey);
             return true;
-        }
-        else
-        {
-            HKEY hKey2;
-            LONG openResult = RegOpenKeyExA(
-                HKEY_LOCAL_MACHINE,
-                "HARDWARE\\DESCRIPTION\\System\\BIOS",
-                0,
-                KEY_READ,
-                &hKey2);
-            if (openResult != ERROR_SUCCESS)
-            {
-                return false;
-            }
-
-            DWORD dataSize = 0;
-            LONG queryResult = RegQueryValueExA(
-                hKey2,
-                "BIOSVersion",
-                NULL,
-                NULL,
-                NULL,
-                &dataSize);
-
-            if (queryResult != ERROR_SUCCESS)
-            {
-                RegCloseKey(hKey2);
-                return false;
-            }
-
-            LPBYTE dataBuffer = new BYTE[dataSize];
-
-            LONG getValueResult = RegGetValueA(
-                hKey2,
-                NULL,
-                "BIOSVersion",
-                RRF_RT_REG_SZ,
-                NULL,
-                dataBuffer,
-                &dataSize);
-            
-            RegCloseKey(hKey2);
-            if (getValueResult != ERROR_SUCCESS)
-            {
-                delete[] dataBuffer;
-                return false;
-            }
-            hKey2 = 0;
-            long driversOpenResult = RegOpenKeyEx(HKEY_LOCAL_MACHINE, TEXT("SOFTWARE\\VMware, Inc.\\VMware Drivers"), 0, KEY_QUERY_VALUE, &hKey2);
-            if (driversOpenResult == ERROR_SUCCESS)
-            {
-                RegCloseKey(hKey2);
-            }
-
-            if (res == ERROR_SUCCESS && (strncmp(reinterpret_cast<const char *>(dataBuffer), "VMW", 3) == 0 || strncmp(reinterpret_cast<const char *>(dataBuffer), "VMware", 6) == 0))
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-            delete[] dataBuffer;
-        }
-        return false;
+        } 
+        return cpuid_check() && bios_version_check();
     }
 
     bool IsMsHyperV()
